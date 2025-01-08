f0de6a77ac8a9473cfcc7fa43b43cfe566056454
Yahmedd <mejbriahmed10@gmail.com>
2025-01-08 23:35:19 +0100

    front

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
-
 // src/Controller/RegistrationController.php
+
 namespace App\Controller;
 
 use App\Entity\User;
@@ -58,24 +58,5 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
-    }
-
-    #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        // Validate the email confirmation link
-        try {
-            $user = $this->getUser();
-            $this->emailVerifier->handleEmailConfirmation($request, $user);
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-            return $this->redirectToRoute('app_register');
-        }
-
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_home');
     }
 }
